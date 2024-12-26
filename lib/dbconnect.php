d420d026eef53f9d8b8e9792e6e62e2e76fc136a
margaritadeligiannidi <margaritadelegiannide@gmail.com>
2024-12-26 14:56:31 +0200

    php file

---
+++ lib/dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+$host='users.iee.ihu.gr';
+$db = 'Ataxx';
+require_once "db_upass.php";
+
+$user=$DB_USER;
+$pass=$DB_PASS;
+
+
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/iee/2021/iee2021034/mysql/run/mysql.sock');
+} else {
+		$pass=null;
+        $mysqli = new mysqli($host, $user, $pass, $db);
+}
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}?>
